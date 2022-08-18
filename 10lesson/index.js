aa176552ec2f3f433f8d153d07d256b89cd06554
GT-Julija <julija.gedgaudiene@gmail.com>
2022-08-18 21:05:36 +0300

    add practice

---
+++ 10lesson/index.js
@@ -0,0 +1,95 @@
+cconst express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const { MongoClient, ObjectId } = require("mongodb");
+const app = express();
+app.use(express.json());
+app.use(cors());
+const port = process.env.PORT || 8080;
+const uri = process.env.CONNECTION;
+const client = new MongoClient(uri);
+
+app.get("/users", async (req, res) => {
+  try {
+    const con = await client.connect();
+
+    const data = await con
+      .db("10paskaita")
+      .collection("users")
+      .find()
+      .toArray();
+    await con.close();
+    return res.send(data);
+  } catch (error) {
+    res.status(500).send({ error });
+  }
+});
+
+app.get("/userscomments", async (req, res) => {
+  try {
+    const con = await client.connect();
+
+    const data = await con
+      .db("10paskaita")
+      .collection("users")
+      .aggregate([
+        // { $match: {} },
+        { $group: { _id: "name" } },
+        {
+          $lookup: {
+            from: "comments", // kolekcija iš kurios nori imti
+            localField: "comment", //  .collection("orders") property
+            foreignField: "name", // from: "customers" property
+            as: "userscomments", // kaip norim pavadinti savo sujungima
+          },
+        },
+      ])
+
+      // .find()
+      // .sort()
+      .toArray();
+    await con.close();
+    return res.send(data);
+  } catch (error) {
+    res.status(500).send({ error });
+  }
+});
+
+app.delete("/users/:id", async (req, res) => {
+  try {
+    const con = await client.connect();
+
+    const data = await con
+      .db("10paskaita")
+      .collection("users")
+      .deleteOne({ _id: ObjectId(req.params.id) });
+
+    await con.close();
+    return res.send(data);
+  } catch (error) {
+    res.status(500).send({ error });
+  }
+});
+
+// 62fe7123e215bd44d5cb7c7d
+// 2022-08-16T16:50:29Z
+// 662fe702de215bd44d5cb7c7a
+
+// app.delete("/users/:email", async (req, res) => {
+//   try {
+//     const con = await client.connect();
+
+//     const data = await con
+//       .db("10paskaita")
+//       .collection("users")
+//       .deleteOne({ email: req.params.email });
+
+//     await con.close();
+//     return res.send(data);
+//   } catch (error) {
+//     res.status(500).send({ error });
+//   }
+// });
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
+
